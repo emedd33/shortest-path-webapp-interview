6709e166d8fe98d5d644b294b9998b9b57e699e6
Eskild Mageli <eskild.emedd33@gmail.com>
2024-06-17 20:33:39 +0200

    Add start and end inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import styles from "./page.module.css";
 import 'reactflow/dist/style.css';
 
-import { Button, ButtonGroup, Input } from "@mui/material";
+import { Alert, Button, ButtonGroup, Input } from "@mui/material";
 import { useEffect, useState } from "react";
 import ReactFlow from "reactflow";
 
@@ -11,17 +11,25 @@ export default function Home() {
   const [graph, setGraph] = useState<null|string[][]>(null);
   const [shortestPath, setShortestPath] = useState<string[] | null>(null);
   const [shortestDistance, setShortestDistance] = useState<number|null>(null);
+  const [startNode, setStartNode] = useState<string|null>(null);
+  const [endNode, setEndNode] = useState<string|null>(null);
   const [reactFlowNodes, setReactFlowNodes] = useState<any>(null);
   const [reactFlowEdges, setReactFlowEdges] = useState<any>(null);
-
+  const [error, setError] = useState<string|null>(null);
   const onClick = () => {
     if (!graph) {
-      console.error("No graph");
+      console.error("Missing graph");
+      setError("Missing graph");
+      return;
+    }
+    if (!startNode || !endNode) {
+      console.error("Missing start or end node");
+      setError("Missing start or end node");
       return;
     }
     fetch("/api/graph", {
       method: "POST",
-      body: JSON.stringify({ startNode: "A", endNode: "L", graph: graph }),
+      body: JSON.stringify({ startNode: startNode, endNode: endNode, graph: graph }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -56,7 +64,7 @@ export default function Home() {
 
       <h1>Shortest Path</h1>
       <ButtonGroup variant="contained" aria-label="Basic button group" >
-        <Button>
+           <Button>
           <input type="file" onChange={(e) => {
             const file = e.target.files?.[0];
             if (file) {
@@ -75,9 +83,14 @@ export default function Home() {
             }
           } } />
         </Button>
-        <Button disabled={!graph} onClick={onClick}>Find path</Button>
-        
+        <Input type="text" placeholder="Start node" onChange={(e=> setStartNode(e.target.value))} style={{backgroundColor: 'Highlight', color: 'white'}}/>
+        <Input type="text" placeholder="End node" onChange={(e=> setEndNode(e.target.value))} style={{backgroundColor: 'Highlight', color: 'white'}}/>
+    
+        <Button disabled={!graph}  onClick={onClick}>Find path</Button>
       </ButtonGroup>
+      {!!error && (
+        <Alert severity="error" style={{display: error ? 'block' : 'none'}}>{error}</Alert>
+      )}
       {reactFlowNodes && reactFlowEdges && (
 
         <div style={{ width: '100%', height: '70vh', backgroundColor: 'white', borderRadius: '10px' }}>
